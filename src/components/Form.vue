09f928407537b8d3ef257c1c1e32a10251bfa708
Alexbelka <alexbelka1992@mail.ru>
2022-12-25 21:31:55 +0300

    validations

---
+++ src/components/Form.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" novalidate>
+
+        <h2>{{form.name}}</h2>
+
+      <b-form-group id="input-group-1" label="Your Name:" label-for="input-1">
+        <b-form-input
+            id="input-1"
+            v-model="form.name"
+            placeholder="Enter name"
+            type="text"
+        ></b-form-input>
+      </b-form-group>
+
+<!--      <b-form-group id="input-group-2" label="Your Surname:" label-for="input-2">-->
+<!--        <b-form-input-->
+<!--            id="input-2"-->
+<!--            v-model="surname"-->
+<!--            placeholder="Enter surname"-->
+<!--        ></b-form-input>-->
+<!--      </b-form-group>-->
+
+<!--      <b-form-group id="input-group-2" label="Your Telephone:" label-for="input-3">-->
+<!--        <b-form-input-->
+<!--            id="input-3"-->
+<!--            v-model="phone"-->
+<!--            placeholder="Enter phone"-->
+<!--            v-mask="'+7(###)-###-##-##'"-->
+<!--        ></b-form-input>-->
+<!--      </b-form-group>-->
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+
+  </div>
+</template>
+
+<script>
+
+import { required } from 'vuelidate/lib/validators'
+
+
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        surname: '',
+        phone: ''
+      }
+    }
+  },
+
+  validations: {
+    name: {
+        required
+      }
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      console.log(this.form.name)
+    },
+
+    onReset(event) {
+      event.preventDefault()
+      this.form.email = ''
+      this.form.name = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vuelidate from 'vuelidate'
+import VueMask from 'v-mask';
+
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(Vuelidate)
+Vue.use(VueMask)
+
 
 new Vue({
   render: h => h(App),
